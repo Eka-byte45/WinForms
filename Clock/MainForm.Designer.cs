b6cd7008c7343edb109ceeeba0b3054a47e83c1f
Екатерина Голумбиевская <golumbiyevskaya90@bk.ru>
2025-12-22 22:10:48 +0800

    HomeWork in progress

--- Clock/MainForm.Designer.cs
+++ Clock/MainForm.Designer.cs
@@ -36,6 +36,17 @@
 			this.cbShowWeekday = new System.Windows.Forms.CheckBox();
 			this.btnHideControls = new System.Windows.Forms.Button();
 			this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+			this.всегдаПоверхОстальныхОконToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.contextMenuStripMain = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.ToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+			this.ToolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+			this.ToolStripMenuItem7 = new System.Windows.Forms.ToolStripMenuItem();
+			this.ToolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+			this.ToolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+			this.ToolStripMenuItem8 = new System.Windows.Forms.ToolStripMenuItem();
+			this.ToolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+			this.contextMenuStripMain.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// labelTime
@@ -48,6 +59,7 @@
 			this.labelTime.Size = new System.Drawing.Size(261, 51);
 			this.labelTime.TabIndex = 0;
 			this.labelTime.Text = "CurrentTime";
+			this.labelTime.MouseDown += new System.Windows.Forms.MouseEventHandler(this.labelTime_MouseDown);
 			this.labelTime.MouseHover += new System.EventHandler(this.labelTime_MouseHover);
 			// 
 			// timer
@@ -94,12 +106,87 @@
 			this.notifyIcon.Text = "System tray";
 			this.notifyIcon.Visible = true;
 			this.notifyIcon.DoubleClick += new System.EventHandler(this.notifyIcon_DoubleClick);
+			this.notifyIcon.MouseDown += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDown);
+			// 
+			// всегдаПоверхОстальныхОконToolStripMenuItem
+			// 
+			this.всегдаПоверхОстальныхОконToolStripMenuItem.Name = "всегдаПоверхОстальныхОконToolStripMenuItem";
+			this.всегдаПоверхОстальныхОконToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+			// 
+			// contextMenuStripMain
+			// 
+			this.contextMenuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem1,
+            this.ToolStripMenuItem4,
+            this.ToolStripMenuItem8,
+            this.ToolStripMenuItem6});
+			this.contextMenuStripMain.Name = "contextMenuStripMain";
+			this.contextMenuStripMain.Size = new System.Drawing.Size(135, 92);
+			// 
+			// ToolStripMenuItem1
+			// 
+			this.ToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem2,
+            this.ToolStripMenuItem3,
+            this.ToolStripMenuItem7});
+			this.ToolStripMenuItem1.Name = "ToolStripMenuItem1";
+			this.ToolStripMenuItem1.Size = new System.Drawing.Size(134, 22);
+			this.ToolStripMenuItem1.Text = "Вид";
+			// 
+			// ToolStripMenuItem2
+			// 
+			this.ToolStripMenuItem2.Name = "ToolStripMenuItem2";
+			this.ToolStripMenuItem2.Size = new System.Drawing.Size(265, 22);
+			this.ToolStripMenuItem2.Text = "Показывать элементы управления";
+			this.ToolStripMenuItem2.Click += new System.EventHandler(this.ToolStripMenuItem2_Click);
+			// 
+			// ToolStripMenuItem3
+			// 
+			this.ToolStripMenuItem3.Name = "ToolStripMenuItem3";
+			this.ToolStripMenuItem3.Size = new System.Drawing.Size(265, 22);
+			this.ToolStripMenuItem3.Text = "Отображать дату";
+			this.ToolStripMenuItem3.Click += new System.EventHandler(this.ToolStripMenuItem3_Click);
+			// 
+			// ToolStripMenuItem7
+			// 
+			this.ToolStripMenuItem7.Name = "ToolStripMenuItem7";
+			this.ToolStripMenuItem7.Size = new System.Drawing.Size(265, 22);
+			this.ToolStripMenuItem7.Text = "Отображать день недели";
+			this.ToolStripMenuItem7.Click += new System.EventHandler(this.ToolStripMenuItem7_Click);
+			// 
+			// ToolStripMenuItem4
+			// 
+			this.ToolStripMenuItem4.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem5});
+			this.ToolStripMenuItem4.Name = "ToolStripMenuItem4";
+			this.ToolStripMenuItem4.Size = new System.Drawing.Size(134, 22);
+			this.ToolStripMenuItem4.Text = "Настройки";
+			// 
+			// ToolStripMenuItem5
+			// 
+			this.ToolStripMenuItem5.Name = "ToolStripMenuItem5";
+			this.ToolStripMenuItem5.Size = new System.Drawing.Size(244, 22);
+			this.ToolStripMenuItem5.Text = "Всегда поверх остальных окон";
+			this.ToolStripMenuItem5.Click += new System.EventHandler(this.ToolStripMenuItem5_Click);
+			// 
+			// ToolStripMenuItem8
+			// 
+			this.ToolStripMenuItem8.Name = "ToolStripMenuItem8";
+			this.ToolStripMenuItem8.Size = new System.Drawing.Size(134, 22);
+			this.ToolStripMenuItem8.Text = "Будильник";
+			// 
+			// ToolStripMenuItem6
+			// 
+			this.ToolStripMenuItem6.Name = "ToolStripMenuItem6";
+			this.ToolStripMenuItem6.Size = new System.Drawing.Size(134, 22);
+			this.ToolStripMenuItem6.Text = "Выход";
+			this.ToolStripMenuItem6.Click += new System.EventHandler(this.ToolStripMenuItem6_Click);
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(348, 362);
+			this.ClientSize = new System.Drawing.Size(316, 352);
 			this.Controls.Add(this.btnHideControls);
 			this.Controls.Add(this.cbShowWeekday);
 			this.Controls.Add(this.cbShowDate);
@@ -110,7 +197,9 @@
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
 			this.Text = "ClockPV_521";
+			this.contextMenuStripMain.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -124,6 +213,16 @@
 		private System.Windows.Forms.CheckBox cbShowWeekday;
 		private System.Windows.Forms.Button btnHideControls;
 		private System.Windows.Forms.NotifyIcon notifyIcon;
+		private System.Windows.Forms.ToolStripMenuItem всегдаПоверхОстальныхОконToolStripMenuItem;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStripMain;
+		private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem1;
+		private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem2;
+		private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem3;
+		private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem4;
+		private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem5;
+		private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem6;
+		private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem7;
+		private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem8;
 	}
 }
 
--- Clock/MainForm.cs
+++ Clock/MainForm.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,21 +17,36 @@ namespace Clock
 		public MainForm()
 		{
 			InitializeComponent();
+			////I вариант
+			//// Получаем размеры экрана
+			//int screenWidth = Screen.PrimaryScreen.Bounds.Width;
+			////this.StartPosition = FormStartPosition.Manual;//если не подключили в визуальном конструкторе среды
+			//// Расчёт позиции окна
+			//this.Left = screenWidth - this.Width; // Ставим окно в правый край экрана
+			//this.Top = 0;                         // Оставляем окно в верхней части экрана
+
+			//II вариант
+			this.Location = new Point(Screen.PrimaryScreen.Bounds.Width - this.Width, 0); // Ставим окно в верхний правый угол экрана
+
 			this.MaximizeBox = false;
-			this.MinimizeBox = false;
+		    this.MinimizeBox = false;
+
 		}
-		void SetVisibility(bool visible)
+		
+		void SetVisibility(bool visible)//Метод управления видимостью элементов
 		{
-			cbShowDate.Visible = visible;
-			cbShowWeekday.Visible = visible;
-			btnHideControls.Visible = visible;
-			this.ShowInTaskbar = visible;
-			this.FormBorderStyle = visible ? FormBorderStyle.FixedSingle : FormBorderStyle.None;
-			this.TransparencyKey = visible ? Color.Empty : this.BackColor;
+			cbShowDate.Visible = visible;//чекбокс дата
+			cbShowWeekday.Visible = visible;//чекбокс день недели
+			btnHideControls.Visible = visible;//кнопка
+			this.ShowInTaskbar = visible;//присутствмие формы в панели задач
+			this.FormBorderStyle = visible ? FormBorderStyle.FixedSingle : FormBorderStyle.None;//FormBorderStyle - стиль рамки окна(FixedSingle - фиксированная рамка вокруг окна
+																								//None - удаляет границу окна и делает ее невидимой
+			this.TransparencyKey = visible ? Color.Empty : this.BackColor;//TransparencyKey - прозрачность окна(Empty - отсутствие прозрачного цвета, то есть эффекта прозрачности не прмиеняется
+																		  //BackColor - присваивает цвет фона самой формы, что делает именно этот цвет прозрачным
 		}
-		private void timer_Tick(object sender, EventArgs e)
+		private void timer_Tick(object sender, EventArgs e)//Обработчик события таймера
 		{
-			labelTime.Text = DateTime.Now.ToString
+			labelTime.Text = DateTime.Now.ToString//Текущее время в форимате ч:м : с AM/PM
 				(
 				"hh:mm:ss tt",
 				System.Globalization.CultureInfo.InvariantCulture
@@ -40,26 +57,70 @@ namespace Clock
 			//	labelTime.Text += DateTime.Now.ToString("yyyy.MM.dd");
 			//}
 			if (cbShowDate.Checked)
-				labelTime.Text += $"\n{DateTime.Now.ToString("yyyy.MM.dd")}";
+				labelTime.Text += $"\n{DateTime.Now.ToString("yyyy.MM.dd")}";//Добавляем дату в формате ГГГГ : ММ : ДД
 			if (cbShowWeekday.Checked)
-				labelTime.Text += $"\n{DateTime.Now.DayOfWeek}";
-			notifyIcon.Text = labelTime.Text ;
+				labelTime.Text += $"\n{DateTime.Now.DayOfWeek}";//Добавляем название текущего дня недели
+			notifyIcon.Text = labelTime.Text ;//Устанавливаем текст уведомления в трей(увидим время на иконке с часами, находящейся на области уведомлений)
+
 		}
 
-		private void btnHideControls_Click(object sender, EventArgs e)
+		private void btnHideControls_Click(object sender, EventArgs e)//Нажатие кнопки btnHidwControls скрывает элементы управления
 		{
 			SetVisibility(false);
 		}
 
-		private void labelTime_MouseHover(object sender, EventArgs e)
+		private void labelTime_MouseHover(object sender, EventArgs e)//Наведение мышью на метку времени labelTime_MouseHover снова показывает элементы управления
 		{
 			SetVisibility(true);
 		}
 
-		private void notifyIcon_DoubleClick(object sender, EventArgs e)
+		private void notifyIcon_DoubleClick(object sender, EventArgs e)//Отображение формы поверх остальных окон
 		{
-			this.TopMost = true;
-			this.TopMost = false;
+			this.TopMost = true;//Формируем сверху всех окон
+			this.TopMost = false;//Возвращаемся к обычному поведению
 		}
+
+		private void labelTime_MouseDown(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Right)
+			{
+				contextMenuStripMain.Show(Cursor.Position); // Показываем контекстное меню
+			}
+		}
+
+		private void ToolStripMenuItem2_Click(object sender, EventArgs e)
+		{
+			SetVisibility(!cbShowDate.Visible && !cbShowWeekday.Visible && !btnHideControls.Visible);
+		}
+
+		private void ToolStripMenuItem3_Click(object sender, EventArgs e)
+		{
+			cbShowDate.Checked = !cbShowDate.Checked;
+		}
+
+		private void ToolStripMenuItem6_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void ToolStripMenuItem5_Click(object sender, EventArgs e)
+		{
+			this.TopMost = !this.TopMost;	
+		}
+
+		private void ToolStripMenuItem7_Click(object sender, EventArgs e)
+		{
+			cbShowWeekday.Checked = !cbShowWeekday.Checked;
+		}
+
+		private void notifyIcon_MouseDown(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Right)
+			{
+				contextMenuStripMain.Show(Cursor.Position); // Показываем контекстное меню
+			}
+		}
+		
 	}
 }
+
