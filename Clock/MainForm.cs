63aba081a1069e30c4d71c88eba08731c5411218
Екатерина Голумбиевская <golumbiyevskaya90@bk.ru>
2025-12-24 21:34:27 +0800

    Home work in progress

--- Clock/MainForm.cs
+++ Clock/MainForm.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,12 +14,18 @@ namespace Clock
 {
 	public partial class MainForm : Form
 	{
+		ColorDialog backgroundColorDialog;
+		ColorDialog foregroundColorDialog;
+		private PrivateFontCollection fontFamilyCollection = new PrivateFontCollection();
 		public MainForm()
 		{
+			
 			InitializeComponent();
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			SetVisibility(false);
+			backgroundColorDialog = new ColorDialog();
+			foregroundColorDialog = new ColorDialog();
 		}
 		void SetVisibility(bool visible)
 		{
@@ -93,8 +101,33 @@ namespace Clock
 			tsmiShowWeekday.Checked = cbShowWeekday.Checked;
 
 		private void tsmiQuit_Click(object sender, EventArgs e) => this.Close();
-		
 
-		
+		private void tsmiForegroundColor_Click(object sender, EventArgs e)
+		{
+			if(foregroundColorDialog.ShowDialog(this) == DialogResult.OK)
+			{
+				labelTime.ForeColor = foregroundColorDialog.Color;
+			}
+
+		}
+
+		private void tsmiBackgroundColor_Click(object sender, EventArgs e)
+		{
+			if(backgroundColorDialog.ShowDialog() == DialogResult.OK)
+			{
+				BackColor = backgroundColorDialog.Color;
+			}
+		}
+
+		private void tsmiFont_Click(object sender, EventArgs e)
+		{
+			string fontFilePath = Path.Combine(Application.StartupPath, "SlackOnChristmasDemoRegular-e95jx.ttf");
+			fontFamilyCollection.AddFontFile(fontFilePath);
+			labelTime.Font = new Font(fontFamilyCollection.Families[0], 35);
+			contextMenuStrip.Font = new Font(fontFamilyCollection.Families[0], 35);
+			btnHideControls.Font = new Font(fontFamilyCollection.Families[0], 35);
+			cbShowDate.Font = new Font(fontFamilyCollection.Families[0], 22);
+			cbShowWeekday.Font = new Font(fontFamilyCollection.Families[0], 22);
+		}
 	}
 }
