a33f4afefbc527c5bacd4d4692de6bec96863717
Екатерина Голумбиевская <golumbiyevskaya90@bk.ru>
2026-01-27 01:14:59 +0800

    add alarms save and load

--- Clock/AlarmsForm.cs
+++ Clock/AlarmsForm.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -55,8 +56,68 @@ namespace Clock
 
 			}
 			writer.Close();
-			Process.Start("notepad", filename);
+			//Process.Start("notepad", filename);
 
+		}
+
+		public void LoadAlarms()
+		{
+			string fileName = Path.Combine(Application.StartupPath, "Alarms.ini");
+			if (!File.Exists(fileName))
+				return;
+			StreamReader reader = new StreamReader(fileName);
+			while (!reader.EndOfStream)
+			{
+				string line = reader.ReadLine().Trim();
+				if (line.Length == 0 || String.IsNullOrWhiteSpace(line)) continue;
+				string[] parts = line.Split('\t');
+				if (parts.Length >= 4)
+				{
+					Alarm alarm = new Alarm();
+					if (parts[0].Equals("Каждый день"))
+						alarm.Date = DateTime.MaxValue;
+					else
+						alarm.Date = DateTime.ParseExact(parts[0], "yyyy.MM.dd", CultureInfo.InvariantCulture);
+					alarm.Time = TimeSpan.Parse(parts[1]);
+					alarm.Days = ParseWeekdays(parts[2]);
+					alarm.Filename = parts[3];
+					listBoxAlarms.Items.Add(alarm);
+				}
+			}
+			reader.Close();
+		}
+
+		private Week ParseWeekdays(string weekdaysStr)
+		{
+			byte daysMask = 0;
+
+			foreach (string part in weekdaysStr.TrimEnd(',').Split(','))
+			{
+				switch (part)
+				{
+					case "Пн": daysMask |= 1 << 0; break;
+					case "Вт": daysMask |= 1 << 1; break;
+					case "Ср": daysMask |= 1 << 2; break;
+					case "Чт": daysMask |= 1 << 3; break;
+					case "Пт": daysMask |= 1 << 4; break;
+					case "Сб": daysMask |= 1 << 5; break;
+					case "Вс": daysMask |= 1 << 6; break;
+				}
+			}
+			return new Week(daysMask);
+		}
+
+		private void buttonDelete_Click(object sender, EventArgs e)
+		{
+			int selectedIndex = listBoxAlarms.SelectedIndex;
+			if (selectedIndex != -1)
+			{
+				listBoxAlarms.Items.RemoveAt(selectedIndex);
+			}
+			else
+			{
+				MessageBox.Show("Сначала выберите будильник.", "Удаление будильника", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
 		}
 	}
 }
